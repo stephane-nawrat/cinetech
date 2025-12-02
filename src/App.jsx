36edffadbd14d9ae9cd9fa64ec605e77882f2b8a
stephane-nawrat <stephane.nawrat@laplateforme.io>
2025-12-02 14:12:43 +0100

    chore: initialisation projet Vite React avec Tailwind et DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-base-100">
+      <h1 className="text-4xl font-bold text-center p-8">
+        Cinétech
+      </h1>
+    </div>
+  )
+}
+
+export default App
